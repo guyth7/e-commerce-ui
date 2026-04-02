718e15e1864949b0240be001dcb0202f392506c7
Guyth <guyth.daba@gamil.com>
2026-04-02 22:54:18 +0300

    feat: integrate react-hook-form and zod for form validation, add cart functionality with zustand, and implement product interaction features
    - Added @hookform/resolvers and zod for form validation in Shipping and Payment forms.
    - Implemented a cart store using zustand for managing cart state.
    - Enhanced ProductCard and ProductInteraction components to handle adding products to the cart.
    - Created a new CartPage component to display cart items and handle checkout steps.
    - Added ShippingForm and PaymentForm components for user input during checkout.
    - Updated Navbar to include a ShoppingCartIcon that displays the number of items in the cart.
    - Refactored product details page to utilize new components and state management.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,15 +1,20 @@
 "use client";
-import { ProductsType } from "@/types";
+
+import useCartStore from "@/stores/cartStore";
+import { ProductType } from "@/types";
 import { ShoppingCart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
-const ProductCard = ({ product }: { product: ProductsType }) => {
-  const [productTypes, setProductTypes] = React.useState({
+const ProductCard = ({ product }: { product: ProductType }) => {
+  const [productTypes, setProductTypes] = useState({
     size: product.sizes[0],
     color: product.colors[0],
   });
+
+  const { addToCart } = useCartStore();
 
   const handleProductType = ({
     type,
@@ -18,22 +23,36 @@ const ProductCard = ({ product }: { product: ProductsType }) => {
     type: "size" | "color";
     value: string;
   }) => {
-    setProductTypes((prev) => ({ ...prev, [type]: value }));
+    setProductTypes((prev) => ({
+      ...prev,
+      [type]: value,
+    }));
   };
+
+  const handleAddToCart = () => {
+    addToCart({
+      ...product,
+      quantity: 1,
+      selectedSize: productTypes.size,
+      selectedColor: productTypes.color,
+    });
+    toast.success("Product added to cart");
+  };
+
   return (
-    <section className="shadow-lg rounded-lg overflow-hidden">
-      {/*IMAGE*/}
+    <div className="shadow-lg rounded-lg overflow-hidden">
+      {/* IMAGE */}
       <Link href={`/products/${product.id}`}>
         <div className="relative aspect-[2/3]">
           <Image
             src={product.images[productTypes.color]}
             alt={product.name}
             fill
-            className="object-cover hover:scale-105 transition-all duration-300 ease-in-out"
+            className="object-cover hover:scale-105 transition-all duration-300"
           />
         </div>
       </Link>
-      {/*PRODUCT DETAILS*/}
+      {/* PRODUCT DETAIL */}
       <div className="flex flex-col gap-4 p-4">
         <h1 className="font-medium">{product.name}</h1>
         <p className="text-sm text-gray-500">{product.shortDescription}</p>
@@ -57,38 +76,44 @@ const ProductCard = ({ product }: { product: ProductsType }) => {
               ))}
             </select>
           </div>
-
           {/* COLORS */}
           <div className="flex flex-col gap-1">
             <span className="text-gray-500">Color</span>
             <div className="flex items-center gap-2">
               {product.colors.map((color) => (
                 <div
+                  className={`cursor-pointer border-1 ${
+                    productTypes.color === color
+                      ? "border-gray-400"
+                      : "border-gray-200"
+                  } rounded-full p-[1.2px]`}
                   key={color}
-                  className={`cursor-pointer border-1 ${productTypes.color === color ? "border-gray-400" : "border-gray-200"} rounded-full p-[1.2px]`}
+                  onClick={() =>
+                    handleProductType({ type: "color", value: color })
+                  }
                 >
                   <div
-                    className={`w-[14px] h-[14px] rounded-full ring-1 ring-gray-300 `}
+                    className="w-[14px] h-[14px] rounded-full"
                     style={{ backgroundColor: color }}
-                    onClick={() =>
-                      handleProductType({ type: "color", value: color })
-                    }
-                  ></div>
+                  />
                 </div>
               ))}
             </div>
           </div>
         </div>
-        {/* PRICE AND ADD TO CART */}
+        {/* PRICE AND ADD TO CART BUTTON */}
         <div className="flex items-center justify-between">
           <p className="font-medium">${product.price.toFixed(2)}</p>
-          <button className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2">
-            <ShoppingCart className="h-4 w-4" />
+          <button
+            onClick={handleAddToCart}
+            className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2"
+          >
+            <ShoppingCart className="w-4 h-4" />
             Add to Cart
           </button>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
---
+++ src/components/ShippingForm.tsx
@@ -0,0 +1,120 @@
+import { ShippingFormInputs, shippingFormSchema } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+const ShippingForm = ({
+  setShippingForm,
+}: {
+  setShippingForm: (data: ShippingFormInputs) => void;
+}) => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ShippingFormInputs>({
+    resolver: zodResolver(shippingFormSchema),
+  });
+  const handleShippingForm: SubmitHandler<ShippingFormInputs> = (data) => {
+    setShippingForm(data);
+    router.push("/cart?step=3", { scroll: false });
+  };
+  return (
+    <form
+      className="flex flex-col gap-4"
+      onSubmit={handleSubmit(handleShippingForm)}
+    >
+      <div className="flex flex-col gap-1">
+        <label htmlFor="name" className="text-xs text-gray-500 font-medium">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          placeholder="John Doe"
+          {...register("name")}
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+        />
+        {errors.name && (
+          <p className="text-xs text-red-500">{errors.name.message}</p>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <label htmlFor="email" className="text-xs text-gray-500 font-medium">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="john.doe@example.com"
+          {...register("email")}
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+        />
+        {errors.email && (
+          <p className="text-xs text-red-500">{errors.email.message}</p>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <label htmlFor="phone" className="text-xs text-gray-500 font-medium">
+          Phone Number
+        </label>
+        <input
+          type="text"
+          id="phone"
+          placeholder="(123) 456-7890"
+          {...register("phone")}
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+        />
+        {errors.phone && (
+          <p className="text-xs text-red-500">{errors.phone.message}</p>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <label htmlFor="address" className="text-xs text-gray-500 font-medium">
+          Address
+        </label>
+        <input
+          type="text"
+          id="address"
+          placeholder="123 Main St, City, State 12345"
+          {...register("address")}
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+        />
+        {errors.address && (
+          <p className="text-xs text-red-500">{errors.address.message}</p>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <label htmlFor="city" className="text-xs text-gray-500 font-medium">
+          City
+        </label>
+        <input
+          type="text"
+          id="New York"
+          placeholder="City"
+          {...register("city")}
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+        />
+        {errors.city && (
+          <p className="text-xs text-red-500">{errors.city.message}</p>
+        )}
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-gray-800 hover:bg-gray-900 transition-all duration-300 ease-in-out text-white p-2 rounded-lg cursor-pointer flex items-center justify-center gap-2"
+      >
+        Continue <ArrowRight className="w-3 h-3" />
+      </button>
+    </form>
+  );
+};
+
+export default ShippingForm;
